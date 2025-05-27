1b2027a4be1dfcbbeb4784880c5012fb16b3f789
jpon0113 <jpongama@gamil.com>
2025-05-27 22:50:30 +0800

    feat: vue init

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>欢迎来到登录界面</h1>
+  <Welcome msg="jack"/>
+  <el-button @click="goHome">回首页</el-button>
+</template>
+
+<script>
+import Welcome from './Welcome.vue'
+export default {
+  name:'login',
+  components:{Welcome},
+  methods:{
+    goHome(){
+      this.$router.push('/welcome')
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
